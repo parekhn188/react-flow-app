6bfcba1aefe96046761d88ef71fc4500c3f926f3
Nisarg <nisargp475@gmail.com>
2023-04-04 20:12:35 -0400

    app js first changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import ReactFlow from 'reactflow'
 
-export default App;
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  {id: '1', position: { x:0, y:0 }, data:{ label: '1' }},
+  {id: '2', position}
+]
