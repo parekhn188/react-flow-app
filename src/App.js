aa9d1a85579dcbbacad5c8b8d1f67b98dc453eea
Nisarg <nisargp475@gmail.com>
2023-04-04 21:59:17 -0400

    custom node

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
-import React from 'react';
-import ReactFlow from 'reactflow'
-
+import { useCallback, useState } from 'react';
+import ReactFlow, { addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow';
 import 'reactflow/dist/style.css';
 
+import TextUpdaterNode from '../src/CustomNodes/TextUpdater';
+
+import './text-updater-node.css';
+
+const rfStyle = {
+  backgroundColor: '#B8CEFF',
+};
+
 const initialNodes = [
-  {id: '1', position: { x:0, y:0 }, data:{ label: '1' }},
-  {id: '2', position: { x:0, y:100}, data:{ label: '2' }},
+  { id: 'node-1', type: 'textUpdater', position: { x: 0, y: 0 }, data: { value: 123 } },
 ];
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
-export default function App() {
+const initialEdges = [
+  { id: 'edge-1', source: 'node-1', sourceHandle: 'a', target: 'node-2' },
+  { id: 'edge-2', source: 'node-1', sourceHandle: 'b', target: 'node-3' },
+];
+
+const nodeTypes = { textUpdater: TextUpdaterNode };
+
+function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
   return (
-    <div style={{width: '100vw', height: '100vh'}}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
-    </div>
-  )
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      nodeTypes={nodeTypes}
+      fitView
+      style={rfStyle}
+    />
+  );
 }
+
+export default Flow;
